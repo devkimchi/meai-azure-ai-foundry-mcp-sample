4c222e00b34615bfce9eabf2abcc439cf3eaa375
Justin Yoo <justin.yoojh@gmail.com>
2025-05-27 00:41:46 +0900

    Initial code commit

---
+++ src/Foundry.ClientApp/Program.cs
@@ -0,0 +1,92 @@
+using System.ClientModel;
+
+using Azure.AI.OpenAI;
+
+using Foundry.ClientApp.Components;
+
+using Microsoft.Extensions.AI;
+
+using ModelContextProtocol.Client;
+using ModelContextProtocol.Protocol;
+
+using OpenAI;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var config = builder.Configuration;
+var connectionstring = config.GetConnectionString("openai") ?? throw new InvalidOperationException("Missing connection string: openai.");
+var endpoint = connectionstring.Split(';').FirstOrDefault(x => x.StartsWith("Endpoint=", StringComparison.InvariantCultureIgnoreCase))?.Split('=')[1]
+                   ?? throw new InvalidOperationException("Missing endpoint.");
+var apiKey = connectionstring.Split(';').FirstOrDefault(x => x.StartsWith("Key=", StringComparison.InvariantCultureIgnoreCase))?.Split('=')[1]
+                 ?? throw new InvalidOperationException("Missing API key.");
+
+builder.AddServiceDefaults();
+
+builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+
+var credential = new ApiKeyCredential(apiKey);
+var openAIOptions = new OpenAIClientOptions()
+{
+    Endpoint = new Uri(endpoint),
+};
+
+var openAIClient = endpoint.TrimEnd('/').Equals("https://models.inference.ai.azure.com")
+                   ? new OpenAIClient(credential, openAIOptions)
+                   : new AzureOpenAIClient(new Uri(endpoint), credential);
+var chatClient = openAIClient.GetChatClient(config["OpenAI:DeploymentName"]!).AsIChatClient();
+
+builder.Services.AddChatClient(chatClient)
+                .UseFunctionInvocation()
+                .UseLogging();
+
+builder.Services.AddSingleton<IMcpClient>(sp =>
+{
+    var config = sp.GetRequiredService<IConfiguration>();
+    var loggerFactory = sp.GetRequiredService<ILoggerFactory>();
+
+    // With .NET Aspire
+    // var uri = new Uri("https+http://mcpserver").Resolve(config);
+
+    // Without .NET Aspire
+    var uri = new Uri(config["McpServers:TodoList:Url"]!);
+
+    var clientTransportOptions = new SseClientTransportOptions()
+    {
+        Endpoint = new Uri($"{uri.AbsoluteUri.TrimEnd('/')}/sse"),
+        AdditionalHeaders = new() { { "x-api-key", config["McpServers:TodoList:ApiKey"]! } }
+    };
+    var clientTransport = new SseClientTransport(clientTransportOptions, loggerFactory);
+
+    var clientOptions = new McpClientOptions()
+    {
+        ClientInfo = new Implementation()
+        {
+            Name = "MCP Todo Client",
+            Version = "1.0.0",
+        }
+    };
+
+    return McpClientFactory.CreateAsync(clientTransport, clientOptions, loggerFactory).GetAwaiter().GetResult();
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseAntiforgery();
+
+app.UseStaticFiles();
+app.MapRazorComponents<App>()
+   .AddInteractiveServerRenderMode();
+
+app.MapDefaultEndpoints();
+
+app.Run();
